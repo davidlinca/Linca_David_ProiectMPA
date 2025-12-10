97b4ded0a08137e8c225a523a5c30a8d518f5fa5
David Linca <alexlinca_21@yahoo.ro>
2025-12-10 18:43:15 +0200

    gRPC

---
+++ Controllers/MovieStatsGrpcController.cs
@@ -0,0 +1,25 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using movieStats;
+
+namespace Linca_David_ProiectMPA.Controllers
+{
+    public class MovieStatsGrpcController : Controller
+    {
+        private readonly MovieStatsService.MovieStatsServiceClient _client;
+
+        public MovieStatsGrpcController()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:7083");
+            _client = new MovieStatsService.MovieStatsServiceClient(channel);
+        }
+
+        public IActionResult Index()
+        {
+            var result = _client.GetMovieCount(new Empty());
+            ViewBag.Count = result.Count;
+            return View();
+        }
+    }
+}
